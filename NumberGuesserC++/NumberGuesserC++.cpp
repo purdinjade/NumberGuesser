136bdb8138bf34a668d1a8cdcc47f189ff91e648
Jade A Purdin <70775363+purdinjade@users.noreply.github.com>
2020-12-28 16:12:34 -0600

    Add files via upload

--- NumberGuesserC++/NumberGuesserC++.cpp
+++ NumberGuesserC++/NumberGuesserC++.cpp
@@ -3,20 +3,21 @@
 
 using namespace std;
 
+//called only in UserSearchForSecretNum function
 void youWon() {
-    cout << "You Won" << endl;
+    cout << "You Won!" << endl;
 }
 
-void UserSearchForSecretNum(int choice) {
+//Function for user to guess the secret number within 10 tries.
+void UserSearchForSecretNum(int secretNum) {
 
-    time_t t;
-    srand((unsigned)time(&t));
-
-    int secretNum = rand() % 100 + 1;
+    int choice;
     int lowest = 1;
     int highest = 100;
-    int mid = secretNum;
     int tries = 0;
+
+    cout << "Enter a Number from 1 - 100" << endl;
+    cin >> choice;
 
     while (tries < 10) {
         if (choice < secretNum) {
@@ -32,6 +33,7 @@ void UserSearchForSecretNum(int choice) {
         else {
             choice == secretNum;
             youWon();
+            cout << "You got it in " << tries << " attempts!" << endl;
             break;
         }
         tries++;
@@ -40,14 +42,64 @@ void UserSearchForSecretNum(int choice) {
 }
 
 
+//Function using a binary algo to figure out the secret number within 10 tries.
+void AlgoSearchForSecretNum(int secretNum) {
+    int lowest = 1;
+    int highest = 100;
+    int mid = lowest + (highest - 1) / 2;
+    int tries = 0;
+    int algoGuess = mid;
+
+    while (tries < 10) {
+        if (algoGuess < secretNum) {
+            cout << "Algo guess " << algoGuess << " was too low" << endl;
+            lowest = mid;
+            mid = ((lowest + 1) + highest) / 2;
+            algoGuess = mid;
+
+        }
+        else if (algoGuess > secretNum) {
+            cout << "Algo guess " << algoGuess << " was too high" << endl;
+            highest = mid;
+            mid = (lowest + (highest - 1)) / 2;
+            algoGuess = mid;
+        }
+        else {
+            algoGuess == secretNum;
+            cout << "Algo guess " << algoGuess << " was right!!" << endl;
+            cout << "Algo got it in " << tries << " attempts!" << endl;
+            cout << "Answer was in fact " << secretNum << endl;
+            break;
+        }
+        tries++;
+    }
+}
+
+//For start of project and rest is in other functions
 int main()
 {
-    int choice;
-    int numbers[100];
+ 
+    int answer;
+    time_t t;
+    srand((unsigned)time(&t));
 
-    cout << "Enter a Number from 1 - 100" << endl;
-    cin >> choice;
+    int secretNum = rand() % 100 + 1;
 
-    UserSearchForSecretNum(choice);
+    do {
+        cout << "Want algo or user to find answer?" << endl;
+        cout << "1. Algo" << endl;
+        cout << "2. User" << endl;
+        cin >> answer;
+
+        if (answer == 1) {
+            AlgoSearchForSecretNum(secretNum);
+        }
+        else if (answer == 2) {
+            UserSearchForSecretNum(secretNum);
+        }
+        else {
+            cout << "Did not get your input" << endl;
+        }
+    } while (answer != 1 && answer != 2);
 
 }
